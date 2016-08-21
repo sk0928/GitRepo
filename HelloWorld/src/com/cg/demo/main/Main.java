2fe980879aaa8ea04678741f4d4273644b3b1b8e
sk0928 <sagar.kharab@gmail.com>
2016-08-21 15:08:51 +0530

    Update Main.java

--- HelloWorld/src/com/cg/demo/main/Main.java
+++ HelloWorld/src/com/cg/demo/main/Main.java
@@ -12,7 +12,7 @@ import jxl.read.biff.BiffException;
 public class ExcelReading {
 
 	public static void main(String[] args) throws BiffException, IOException {
-		String response = "JJJJAAAANNNNUUUUUAAAAARRRRRYYYYFFFFEEEBBBRRRUUUUAAAARRRRYYYYMMMMMMAAAAAARRRRRRRCCCCCCHHHHHHAAAAAAPPPPPPRRRRRRIIIIIILLLLLLMMMMMMMMMMAAAAAAAAAAAYYYYYYYYYYJJJJJJJUUUUUUUUNNNNNNNNEEEEEEEJJJJJJJUUUUUUUULLLLLLLLYYYYYYYYAAAAAUUUUUGGGGGUUUUUUSSSSSTTTTTSSSSEEEEPPPTTTEEEMMMBBBEEERRRROOOOCCCCCTTTTTOOOOOBBBBEEEERRRRNNNNOOOOVVVVEEEMMMBBBBEEEERRRRDDDDEEEECCCCEEEMmMMBBBBEEEERRRR";
+		String response = "JJJJAAAANNNNUUUUUAAAAARRRRRYYYYFFFFEEEBBBRRRUUUUAAAARRRRYYYYMMMMMMAAAAAARRRRRRRCCCCCCHHHHHHAAAAAAPPPPPPRRRRRRIIIIIILLLLLLMMMMMMMMMMAAAAAAAAAAAYYYYYYYYYY002JJJJJJJUUUUUUUULLLLLLLLYYYYYYYYAAAAAUUUUUGGGGGUUUUUUSSSSSTTTTTSSSSEEEEPPPTTTEEEMMMBBBEEERRRRJJJJJJJUUUUUUUULLLLLLLLYYYYYYYYAAAAAUUUUUGGGGGUUUUUUSSSSSTTTTTSSSSEEEEPPPTTTEEEMMMBBBEEERRRROOOOCCCCCTTTTTOOOOOBBBBEEEERRRRNNNNOOOOVVVVEEEMMMBBBBEEEERRRRDDDDEEEECCCCEEEMmMMBBBBEEEERRRR";
 
 		List<MessageName> messagename = new ExcelReading().excel();
 		new ExcelReading().stringprint(response, messagename);
@@ -22,36 +22,41 @@ public class ExcelReading {
 		int count = 0;
 		int level = 0;
 		for (int i = 0; i < messageName.size(); i++) {
-			if ((null != messageName.get(i))&& (messageName.get(i).getName().contains("Jun") || messageName.get(i).getName().contains("jun"))) {
+			if ((null != messageName.get(i))
+					&& (messageName.get(i).getName().contains("Jun") || messageName.get(i).getName().contains("jun"))) {
 				int ctemp = 0;
-				System.out
-						.println(messageName.get(i).getName() + ":" + str.substring(0, messageName.get(i).getLength()));
+				messageName.get(i).setValue(str.substring(0, messageName.get(i).getLength()));
+				System.out.println(messageName.get(i).getValue());
+				int Counter = Integer.parseInt(messageName.get(i).getValue());
 				str = str.substring(messageName.get(i).getLength());
-				/*messageName.remove(i);
-				messageName.add(i, null);*/
+				/*
+				 * messageName.remove(i); messageName.add(i, null);
+				 */
 				if (ctemp == 0) {
 					level = messageName.get(i + 1).getLevel();
 				}
-				for (int temp = 0; temp <= messageName.get(i).getLength(); temp++) {
-				while (messageName.get(i + 1).getLevel() == (messageName.get(i).getLevel())) {
-						System.out.println("hello");
-						i++;
-						}
+				int temp_index=i+1;
+				for (int temp = 0; temp <= Counter; temp++) {
+					while(messageName.get(temp_index).getLevel()==level){
+						System.out.println("Hello");
+						temp_index++;
+					}
+					temp_index=i+1;
 				}
 
 				count++;
 
 			} else {
 				if (count == 0) {
-					System.out.println(
-							messageName.get(0).getName() + ":" + str.substring(0, messageName.get(0).getLength()));
+					messageName.get(i).setValue(str.substring(0, messageName.get(0).getLength()));
+					System.out.println(messageName.get(i).getValue());
 					str = str.substring(messageName.get(0).getLength());
 
 					count++;
 				} else {
 					String str1 = str;
-					System.out.println(
-							messageName.get(i).getName() + ":" + str.substring(0, messageName.get(i).getLength()));
+					messageName.get(i).setValue(str.substring(0, messageName.get(i).getLength()));
+					System.out.println(messageName.get(i).getValue());
 					str = str.substring(messageName.get(i).getLength());
 
 				}
